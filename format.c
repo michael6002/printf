dd69e88a418a9100bd7b579b4f843fd7d0b7af9a
root <root@LAPTOP-6OFKQM5A>
2023-05-31 21:17:57 +0200

    added format c program

---
+++ format.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main () {
+        int Ganzzahl = 56789;
+        float Gleitkommazahl = 567.89;
+        char Character = 'b';
+        char String[]="Das ist ein String";
+
+        printf("Ganzzahl: %i \n", Ganzzahl);
+        printf("Gleitkommazahl: %.2f\n", Gleitkommazahl);
+        printf("Character: %c \n", Character);
+        printf("String: %s \n", String);
+
+        return 0;
+}
